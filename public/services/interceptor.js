33b539812c31aaab5c0177c2f813dcb56f92cec0
Tim Elfelt <timelf123@gmail.com>
2016-07-21 11:38:01 -0500

    log interceptor

--- public/services/interceptor.js
+++ public/services/interceptor.js
@@ -1,40 +1,88 @@
 'use strict';
 
+
+
 angular.module('mean-factory-interceptor', [])
+
   .factory('httpInterceptor', ['$q', '$location', '$meanConfig', '$cookies', 'PlatformSetting',
+
     function($q, $location, $meanConfig, $cookies, PlatformSetting) {
 
+
+
       return {
+
         'response': function(response) {
 
+
+
           if (response.status === 401) {
+
+            console.log('response 401', response);
+
+            console.log('redirecting to loginPage', $meanConfig.loginPage);
+
             $location.url($meanConfig.loginPage);
+
             return $q.reject(response);
+
           }
+
           return response || $q.when(response);
+
         },
+
+
 
         'responseError': function(rejection) {
 
+
+
           if (rejection.status === 401) {
 
+            console.log('responseError 401', rejection);
+
+
+
             // This is to set the cookie so that we can redirect back to the proper urL
+
+            console.log('droppin cookies for login redirect', $location.path());
+
             $cookies.put('redirect', $location.path());
+
+            console.log('redirecting to loginPage', $meanConfig.loginPage);
 
             $location.url($meanConfig.loginPage);
 
+
+
             return $q.reject(rejection);
 
+
+
           }
+
           return $q.reject(rejection);
+
         }
 
+
+
       };
+
     }
+
   ])
+
   //Http Interceptor to check auth failures for XHR requests
+
   .config(['$httpProvider',
+
     function($httpProvider) {
+
       $httpProvider.interceptors.push('httpInterceptor');
+
     }
+
   ]);
+
